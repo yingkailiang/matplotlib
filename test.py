66a67242e6e23a697a26b2231574ce6d968ee584
yingkailiang <yingkailiang@yingkailiang-ND10.(none)>
2015-09-07 00:18:12 -0400

    clean the graph code

--- test.py
+++ test.py
@@ -5,19 +5,23 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-x = np.linspace(0, 1, 100)
-y = np.sin(x * 2 * np.pi) / 2 + 0.5
-ax.plot(x, y, zs=0, zdir='z', label='zs=0, zdir=z')
+#x = np.linspace(0, 1, 100)
+#y = np.sin(x * 2 * np.pi) / 2 + 0.5
+#ax.plot(x, y, zs=0, zdir='z', label='zs=0, zdir=z')
 
 colors = ('r', 'g', 'b', 'k')
 for c in colors:
-    x = np.random.sample(20)
-    y = np.random.sample(20)
-    ax.scatter(x, y, 0, zdir='y', c=c)
-
+    #x = np.random.sample(20)
+    #y = np.random.sample(20)
+    x = 200
+    y = 200
+    z = 200
+    print("X:",x,",Y:",y)
+    #ax.scatter(x, y, 0, zdir='y', c=c)
+    ax.scatter(x, y, z, zdir='y', c=c)
 ax.legend()
-ax.set_xlim3d(0, 1)
-ax.set_ylim3d(0, 1)
-ax.set_zlim3d(0, 1)
+ax.set_xlim3d(0, 1000)
+ax.set_ylim3d(0, 1000)
+ax.set_zlim3d(0, 1000)
 
 plt.show()
