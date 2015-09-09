989ef3896df3ad556667bf326b2349fce39b04f9
Seed <seed@ubuntu.(none)>
2015-09-09 15:36:58 -0700

    hw function

--- test.py
+++ test.py
@@ -4,33 +4,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-#generate n dimensional random number array
-def genRand(n):
+#generate d dimensional random number array
+def genRand(d):
     result=[] 
-    for i in range(0,n):
+    for i in range(0,d):
   	r = randint(1,100)
         result.append(r)
- 	print "random number ",i,": ",r
+ 	#print "random number ",i,": ",r
     return result
 
-#p, q are coordinate array, n is the number of dimension 
-def euclideanDistance(p,q,n):
+#p, q are coordinate array, d is the number of dimension 
+def euclideanDistance(p,q,d):
     innerSum = 0
-    for i in range(0,n):
+    for i in range(0,d):
         innerSum = innerSum + (p[i] - q[i])*(p[i] - q[i])
     return math.sqrt(innerSum)
 
-genRand(5)
+#max from array
+def maxInArray(a):
+    result = a[0] 
+    for ai in a:
+	if result < ai:
+	    result = ai	
+    return result
+
+#max from array
+def minInArray(a):
+    result = a[0] 
+    for ai in a:
+	if result > ai:
+	    result = ai	
+    return result
+
+#compute the variance
+def gema(maxv,minv):
+    tmp = (maxv-minv)/minv 
+    return math.log(tmp,2)
+
+#main function start here: 
+d = 100
+n = 100
+distance=[]
+for i in range (0,n):
+    point1 = genRand(d)
+    point2 = genRand(d)
+    distance.append(euclideanDistance(point1,point2,d))
+variance = gema(maxInArray(distance),minInArray(distance))
+
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-
 colors = ('r', 'g', 'b', 'k')
 for c in colors:
     #x = np.random.sample(20)
     #y = np.random.sample(20)
-    x = 200
-    y = 200
-    z = 200
+    x = d 
+    y = n  
+    z = variance 
     #print "X:",x,",Y:",y 
     #ax.scatter(x, y, 0, zdir='y', c=c)
     ax.scatter(x, y, z, zdir='y', c=c)
